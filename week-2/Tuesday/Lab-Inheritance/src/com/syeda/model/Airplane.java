6d3a0f8f09495fc7fa28090feb06e6d5e13b42c2
Sidd <sulsyeda03@gmail.com>
2021-07-22 11:12:03 -0500

    Adding week 2 labs

---
+++ week-2/Tuesday/Lab-Inheritance/src/com/syeda/model/Airplane.java
@@ -0,0 +1,24 @@
+package com.syeda.model;
+
+public class Airplane extends FlyingVehicle {
+    private short wheels;
+    private short propellers;
+    
+    //constructors
+    public Airplane(short wheels, short propellers) {
+    	super();
+        this.wheels = wheels;
+        this.propellers = propellers;
+    }
+
+    //getters and setters
+    public short getWheels(){ return this.wheels; }
+    public void setWheels(short wheels){ this.wheels = wheels; }
+
+    public short propellors() { return this.propellers; }
+    public void setPropellers(short propellers) {this.propellers = propellers; }
+
+    public void turn(){
+        System.out.println("Turning...");
+    }
+}
---
+++ week-2/Tuesday/Lab-Inheritance/src/com/syeda/model/Engine.java
@@ -0,0 +1,12 @@
+package com.syeda.model;
+
+public class Engine {
+
+    public void on() {
+        System.out.println("Turning engine on");
+    }
+
+    public void off(){
+        System.out.println("Turning engine off");
+    }
+}
